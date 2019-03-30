46da48c9618462b2229329a2a8b24379dda4acca
Administrator <palki3289@gmail.com>
2019-03-30 10:49:22 -0700

    Inital commit with Book class

---
+++ Library/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Book
+    {
+        #region Properties
+        /// <summary>
+        /// Defines title of book
+        /// </summary>
+        public string Title { get; set; }
+       
+        /// <summary>
+        /// defines the pagecount of book
+        /// </summary>
+        public int PageCount { get; set; }
+       
+        /// <summary>
+        /// whether book is checkedout or not
+        /// </summary>
+        public bool Checkout { get; set; }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
